bf2776e6449486e1bb38a3a401170ac048741eca
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2025-06-01 07:37:22 +0700

    tried basic stuff of react native

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome6 } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Button,
+  Linking,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const name = "Reza Fauzi Augusdi";
+  const links = {
+    github: "fbtwitter",
+    email: "rzaugusdi354@gmail.com",
+    x: "rzaugusdi",
+  };
+  const getOccupation = () => {
+    return "Full-stack Developer";
+  };
+
+  const getIcons = () => {
+    return (
+      <View style={{ flexDirection: "row", marginVertical: 10, gap: 10 }}>
+        {links.github && <FontAwesome6 name="github" size={24} color="black" />}
+        {links.x && <FontAwesome6 name="x-twitter" size={24} color="black" />}
+        {links.email && <FontAwesome6 name="at" size={24} color="black" />}
+      </View>
+    );
+  };
+  const onContactMe = () => {
+    Linking.openURL("mailto:rzaugusdi354@gmail.com");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView edges={["bottom"]}>
+        <ScrollView>
+          <View style={styles.container}>
+            <Image
+              source={{
+                uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg",
+              }}
+              style={{ width: "100%", aspectRatio: 16 / 9 }}
+            />
+            <Image
+              source={require("./assets/reza.jpeg")}
+              style={{
+                width: 150,
+                height: 150,
+                borderRadius: 150,
+                borderWidth: 5,
+                borderColor: "white",
+                marginTop: -75,
+              }}
+            />
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>{name}</Text>
+            <Text>{getOccupation()}</Text>
+            {getIcons()}
+            <Button title="Contact me" onPress={onContactMe} />
+
+            <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+
+            <StatusBar style="auto" />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
